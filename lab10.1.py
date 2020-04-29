3b91db4ed22e6f065d7accc0dd44644c9c8e94ca
CodeLyokoZ <59044698+CodeLyokoZ@users.noreply.github.com>
2020-04-29 13:34:57 +0530

    Update lab10.1.py

--- lab10.1.py
+++ lab10.1.py
@@ -16,6 +16,11 @@ def add_bst(i, index = 0): #funtion for add value to binary search tree
     l_nod = (index * 2 + 1) # left nod index value
     r_nod = (index * 2 + 2) # right nod index value
     
+    try:
+        out_list[index]
+    except:
+        out_list += ['null' for _ in range(index - len(out_list) + 1)]
+    
     if out_list[index] == 'null': # if position is empty assign nod value
         out_list[index] = i
     else:
@@ -28,15 +33,10 @@ filename = input() # get file name from terminal
 main_list = read_file(filename) # get user file content as list
 
 list_len = len(main_list) # Get length of list
-out_list = ['null' for _ in range(2**(list_len//2))] # create a empty value complete tree for store value using list
+out_list = [] # create a empty tree list
 
 for i in main_list: # get each values in list
     add_bst(i) # send values to tree for assign it to propper nod
 
-for _ in range(1, len(out_list)): # check each element in tree
-    if out_list[-1] == 'null': # check if unwanted leaf in tree
-        out_list.pop() # pop those unwanted leaf
-    else:
-        break # if finifh the pop for unwanted leaf break poping leaf
 content = ' '.join(list(map(str, out_list))) # create out string for output
 save_file('result.txt', content) # save the content
